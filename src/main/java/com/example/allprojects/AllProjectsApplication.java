d91b7d186c65d1e66ef269e2fe300524f0147616
ThiruRR <thirumaniraj_rr@trimble.com>
2021-08-05 13:54:46 +0530

    Initial commit

---
+++ src/main/java/com/example/allprojects/AllProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.allprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/allprojects/AllProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.allprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
